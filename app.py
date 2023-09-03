b1c3b7704ba27b59c8a39eddaad0c47802656711
dimitreOliveira <dimitreandrew@gmail.com>
2023-09-03 20:33:58 -0300

    Adding configs

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 import google.generativeai as palm
+import yaml
 from dotenv import load_dotenv
 
 logging.basicConfig(level=logging.INFO)
@@ -10,33 +11,16 @@ load_dotenv()
 
 # API_KEY loaded from the `.env` file which is not visible for obvious reasons
 palm.configure(api_key=os.getenv("API_KEY"))
+CONFIGS_PATH = "configs.yaml"
 
-defaults = {
-    "model": "models/text-bison-001",
-    "temperature": 0.7,
-    "candidate_count": 1,
-    "top_k": 40,
-    "top_p": 0.95,
-    "max_output_tokens": 1024,
-    "stop_sequences": [],
-    "safety_settings": [
-        {"category": "HARM_CATEGORY_DEROGATORY", "threshold": 4},
-        {"category": "HARM_CATEGORY_TOXICITY", "threshold": 4},
-        {"category": "HARM_CATEGORY_VIOLENCE", "threshold": 4},
-        {"category": "HARM_CATEGORY_SEXUAL", "threshold": 4},
-        {"category": "HARM_CATEGORY_MEDICAL", "threshold": 4},
-        {"category": "HARM_CATEGORY_DANGEROUS", "threshold": 4},
-    ],
-}
+configs = yaml.safe_load(open(CONFIGS_PATH, "r"))
+logger.info(f"Configs: {configs['defaults']}")
+logger.info(f"Context: {configs['context']}")
 
-CONTEXT = """
-You are a content creation helper who helps to turn technical concepts into short text easier to understand.
-Explain the following concept in a short text format, and give some examples:
-"""
-
-prompt = f"""{CONTEXT} Machine learning models usually overfit small datasets"""
-
+prompt = (
+    f"""{configs['context']} Machine learning models usually overfit small datasets"""
+)
 logger.info(f"Prompt: {prompt}")
 
-response = palm.generate_text(**defaults, prompt=prompt)
+response = palm.generate_text(**configs["defaults"], prompt=prompt)
 logger.info(f"Prompt: {response.result}")
