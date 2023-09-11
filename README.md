704e28979b4899b2f2b3e8bced02dfddffed7556
dimitreOliveira <dimitreandrew@gmail.com>
2023-09-10 21:32:15 -0300

    Updating prompt and refacoting

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
-# palm-content-creation
-Content creation with PaLM API and Streamlit
+# PaLM-based content creation
+Create short text content with PaLM API and Streamlit
+
+# Usage
+
+Start Streamlit app
+```bash
+make app
+``````
+
+Run linter and formatter
+```bash
+make lint
+```
+
+# Resources
+- [MakerSuite](https://makersuite.google.com/app/home)
+- [MakerSuite - Guide](https://developers.generativeai.google/guide)
+- [MakerSuite expansion announcement](https://developers.googleblog.com/2023/08/makersuite-expands-adds-new-features-for-ai-makers.html)
+- [Streamlit](https://streamlit.io/)
+
 
 TODO
 - Add tests
-- Add multiple outputs (2 or 3)
-- Add [better}(https://extras.streamlit.app/Keyup%20text%20input) looking [element to outputs](https://docs.streamlit.io/library/api-reference/layout)
 - Add [github hooks](https://github.com/dimitreOliveira/stable-diffusion-textual-inversion-app/tree/main/.github/workflows)
-- Add [input elements](https://docs.streamlit.io/library/api-reference/widgets) to control the defaults
--- app.py
+++ app.py
@@ -6,6 +6,11 @@ import streamlit as st
 import yaml
 from dotenv import load_dotenv
 
+
+CONTEXT = """"
+Explain the topic of "{}" in a short text format that is targeted at the general public. The tone of the text should be informative and easy to understand with some examples.
+﻿"""
+
 st.set_page_config(
     page_title="PaLM-based content creation",
     page_icon="🌴",
@@ -15,8 +20,7 @@ st.set_page_config(
 @st.cache_resource()
 def parse_configs(configs_path: str):
     configs = yaml.safe_load(open(configs_path, "r"))
-    logger.info(f"Configs: {configs['defaults']}")
-    logger.info(f"Context: {configs['context']}")
+    logger.info(f"Configs: {configs}")
     return configs
 
 
@@ -27,7 +31,7 @@ def setup_keys():
 
 def query(prompt: str, defaults: dict):
     response = palm.generate_text(**defaults, prompt=prompt)
-    return response.result
+    return response
 
 
 # API_KEY loaded from the `.env` file which is not visible for obvious reasons
@@ -43,14 +47,27 @@ setup_keys()
 configs = parse_configs(CONFIGS_PATH)
 
 st.title("PaLM-based content creation")
-input = st.text_input("Enter a topic to generate content")
+input = st.text_input(label="Enter a topic to generate content")
 
-prompt = f"{configs['context']} {input}"
+prompt = CONTEXT.format(input)
 logger.info(f"Prompt: {prompt}")
 
 generate = st.button("Generate")
 
 if generate:
     with st.spinner("Generating content..."):
-        response = query(prompt, configs["defaults"])
-    st.write(response)
+        response = query(prompt, configs)
+
+    tab1, tab2, tab3 = st.tabs(["Content #1", "Content #2", "Content #3"])
+
+    with tab1:
+        st.header("Generated content #1")
+        st.write(response.candidates[0]["output"])
+
+    with tab2:
+        st.header("Generated content #2")
+        st.write(response.candidates[1]["output"])
+
+    with tab3:
+        st.header("Generated content #3")
+        st.write(response.candidates[2]["output"])
